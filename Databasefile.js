b8dcdcd442905427b7d0ce7d71a3242e827310c6
Abhishek Makode <abhishekmakode412@gmail.com>
2026-08-29 15:06:59 +0530

    update

--- Databasefile.js
+++ Databasefile.js
@@ -1,5 +1,8 @@
 const mongoose = require("mongoose")
-const mongoURL = "mongodb://localhost:27017/schooldb"
+const dotenv = require("dotenv")
+dotenv.config()
+const mongoURL = process.env.MONGODB_URL
+
 mongoose.connect(mongoURL).then(
     ()=>{
         console.log("server is connected")
--- Serverfile.js
+++ Serverfile.js
@@ -4,9 +4,12 @@ const Databasefile = require("./Databasefile")
 const Studentroutes = require("./Studentsroutes")
 const Teacherroutes = require("./Teacherroutes")
 
+const dotenv = require("dotenv")
+dotenv.config()
 const bodyParser = require('body-parser')
 const app = express()
 app.use(bodyParser.json())
+const PORT = process.env.PORT || 4000;
 
 app.get("/" , (req ,res)=>{
       console.log("hello abhishek")
@@ -16,7 +19,7 @@ app.get("/" , (req ,res)=>{
 app.use("/student" , Studentroutes)
 app.use("/teacher" , Teacherroutes)
  
-app.listen( 4000 , ()=>{
+app.listen( PORT , ()=>{
     console.log("server is connected")
 
 }
